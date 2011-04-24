d8f7a6a9cefff07c8415baa8445d587f6c772b1c
max__k <max__k@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-24 22:16:17 +0000

    git-svn-id: http://plugins.svn.wordpress.org/instagramwidget@376660 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/instagramWidget.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+  Plugin Name: InstagramWidget
+  Description: Shows the last X pictures from your instagram-feed
+  Author: Max Kleucker
+  Version: 0.9
+  Author URI: http://www.justcurious.de
+*/
+/*
+  Instagram Widget: Holt die letzten Bilder von Instagram und zeigt sie in einem Widgte an.
+  Dazu eine einfache Verlinkung, aber keine weiterer SchnickSchnack.
+  Cachet die unter uploads/instagram und velrinkt zu den Detailaufnahmen.
+*/
+
+
+class InstagramWidget extends WP_Widget {
+
+  
+  /**
+   * WP-Widget Constructor
+   */
+	function InstagramWidget() {
+	
+		parent::WP_Widget(false, $name = 'InstagramWidget');
+		
+	}
+	
+
+  /**
+   * The loop for displaying all the images.
+   */
+	function widget($args, $instance) 
+	{
+	
+	  // Loading the JSON Data
+    $json = json_decode($this->getData($instance['access_token'], $instance['count_images'], $instance['cache_time']), true);
+    
+    // Showing sourrounding div
+		echo '<div>';
+		
+		// The loop
+		foreach ($json["data"] as $value) 
+		{ 
+      echo $this->echoimage($value, $instance['image_size']); 
+    }
+    
+		echo '</div>';
+	}
+
+
+  /**
+   * Updating the InstagramWidget Instance and resetting the Cache
+   */
+	function update($new_instance, $old_instance) {
+		// Speichern der Optionen
+		$instance = $old_instance;
+		$instance['access_token'] = $new_instance['access_token'];
+		$instance['count_images'] = $new_instance['count_images'];
+		$instance['cache_time'] = $new_instance['cache_time'];
+		$instance['image_size'] = $new_instance['image_size'];
+		
+		$this->resetCache();
+		
+		return $new_instance;
+	}
+
+	
+	
+	/**
+	 Gets the Data from either the Cache or the API
+	*/
+	function getData($token, $number, $cache_time){
+    
+    $wp_uploads = wp_upload_dir();
+    
+    $cache_folder = $wp_uploads['basedir']. "/instagram/";
+    
+    if(!empty($wp_uploads['error'])) echo $wp_uploads['error'];
+    
+    if(!is_dir($cache_folder)){
+      if(mkdir($cache_folder, 0777) == false){
+        return $this->getDataFromApi($token, $number);
+      }
+    }
+    
+    $cachefile = $cache_folder."user.cache";
+    
+    if (file_exists($cachefile) && time()-filemtime($cachefile)<$cache_time) {
+      try{
+        $contents = file_get_contents($cachefile);
+      }catch(Exception $e){
+        $contents = $this->getDataFromApi($token, $number);
+        file_put_contents($cachefile, $contents);
+      }
+      
+    } else {
+      $contents = $this->getDataFromApi($token, $number);
+      file_put_contents($cachefile, $contents);
+    }
+    
+    return $contents;
+	}
+	
+	
+	/**
+	 Getting the Data from the API
+	*/
+	function getDataFromApi($token, $number){
+    // Instagram API bearbeiten
+    $contents = file_get_contents("https://api.instagram.com/v1/users/self/media/recent/?access_token=$token&count=$number");
+    return $contents;
+	}
+	
+	/**
+	 Displaying the single images.
+	*/
+	function echoimage($val, $imagesize) {
+ 
+    $image = $val["images"][$imagesize]["url"];
+    $link = $val["link"];
+   
+    return "<a href=\"$link\"><img src=\"$image\"/></a>";
+   
+  }
+  
+  /** Not yet functional */
+  function resetCache(){
+  
+  }
+  
+  /**
+   * Parsing the Form for the Admin-Area.
+   */
+	function form($instance) {
+		// Formular des Widgets im Backend
+		$defaults = array('numberphotos'=>'3');
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = "instagram stream";
+?>
+    <div>
+    <label for="<?php echo $this->get_field_id('access_token'); ?>">Access Token:</label>
+      <input type="text" name="<?php echo $this->get_field_name('access_token') ?>" id="<?php echo $this->get_field_id('access_token') ?> " value="<?php echo $instance['access_token'] ?>" size="20"/>
+      <p style="font-size: 0.86em;">For getting an Access Token please <a href="http://justcurious.de/projects/instagramwidget/apiaccess.php" target="_blank">click here</a>.</p>
+    </div>  
+    <div>
+    <label for="<?php echo $this->get_field_id('count_images'); ?>">Count Images:</label>
+      <input type="text" name="<?php echo $this->get_field_name('count_images') ?>" id="<?php echo $this->get_field_id('count_images') ?> " value="<?php echo $instance['count_images'] ?>" size="3"/>
+    </div> 
+    <div> 
+    <label for="<?php echo $this->get_field_id('cache_time'); ?>" title="How long are API results cached?">Cache:</label>
+      <input type="text" name="<?php echo $this->get_field_name('cache_time') ?>" id="<?php echo $this->get_field_id('cache_time') ?> " value="<?php echo (empty($instance['cache_time']))? '600' : $instance['cache_time']; ?>" size="3"/> (ms)
+    </div>
+    <div>
+    <label for="<?php echo $this->get_field_id('image_size'); ?>">Image Size:</label>
+      <select name="<?php echo $this->get_field_name('image_size') ?>" id="<?php echo $this->get_field_id('image_size') ?> ">
+        <option value="thumbnail" <?php if($instance['image_size'] == "thumbnail") echo 'selected="selected"'; ?>>Thumbnail (150x150)</option>
+        <option value="low_resolution" <?php if($instance['image_size'] == "low_resolution") echo 'selected="selected"'; ?>>Low Res (306x306)</option>
+        <option value="standard_resolution" <?php if($instance['image_size'] == "standard_resolution") echo 'selected="selected"'; ?>>Thumbnail (612x612)</option>
+      </select>
+      
+    </div>
+<?php
+	}
+  
+  
+}
+add_action('widgets_init', create_function('', "register_widget('InstagramWidget');"));
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,45 @@
+=== Plugin Name ===
+Contributors: max__k
+Tags: instagram, widget, photos
+Requires at least: 2.8
+Tested up to: 3.1
+Stable tag: 0.9
+
+Shows your latest Instagram photos in a widget.
+
+== Description ==
+
+This plugin shows your latest Instagram pictures in a widget which you can include in your sidebar. 
+This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
+
+
+== Installation ==
+
+= Requirements =
+* Widget-able Wordpress theme
+* Please make sure, `PHP_SAFE_MODE` is turned off by your webhoster
+* To enable Cache, the directory `{upload-folder}/instagram/` (there won't be stored more than 10 KB)
+
+= Installation =
+1. Upload `instagramWidget.php` to the `/wp-content/plugins/` directory
+1. Enable the plugin through the Plugin-Section
+1. Add the widget to any widget-area you want to
+1. Make sure you provide an API Key
+
+
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
+2. This is the second screen shot
+
+== Changelog ==
+
+= 0.9 =
+* First Version added
+
